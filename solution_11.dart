89d8f0f8b14b59ae579bbc70a9eb1d132e2b9a46
MohammadAsher <155186559+MohammadAsher@users.noreply.github.com>
2024-02-11 13:48:15 -0800

    Add files via upload

---
+++ solution_11.dart
@@ -0,0 +1,13 @@
+//Question-11
+
+void main() {
+  List numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List newList = [];
+  var n = 08;
+
+  for (var i = 0; i < n; i++) {
+    newList.add(numbers[i]);
+  }
+  print(numbers);
+  print(newList);
+}
